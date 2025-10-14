a6e7d3f1005e84b572a38c0dda3932cdeb3eab17
Alexander Castronovo <alexandercastronovo@gmail.com>
2025-10-14 12:08:47 -0400

    test

--- install.bat
+++ install.bat
@@ -11,15 +11,16 @@ if %errorlevel% neq 0 (
 
 set "SCRIPT_DIR=%~dp0"
 
-set "DRAGONFLY_INSTALL_PATH=C:\ProgramData\ORS\Dragonfly2024.1"
+set "DRAGONFLY_PROGRAMDATA=C:\ProgramData\Comet\Dragonfly2025.1"
+set "DRAGONFLY_INSTALL=C:\Program Files\Dragonfly"
 
 :: Copy files
 echo Copying files...
-xcopy "%SCRIPT_DIR%*.*" "%DRAGONFLY_INSTALL_PATH%\pythonAllUsersExtensions\Plugins\DSB_efd060071a1711f0b40cf83441a96bd5" /E /I /Y >nul
+xcopy "%SCRIPT_DIR%*.*" "%DRAGONFLY_PROGRAMDATA%\pythonAllUsersExtensions\Plugins\DSB_efd060071a1711f0b40cf83441a96bd5" /E /I /Y >nul
 
 :: Install Python dependencies
 echo Installing Python dependencies...
-"%DRAGONFLY_INSTALL_PATH%\Anaconda3\python.exe" -m pip install -r "%DRAGONFLY_INSTALL_PATH%\pythonAllUsersExtensions\Plugins\DSB_efd060071a1711f0b40cf83441a96bd5\requirements.txt"
+"%DRAGONFLY_INSTALL%\Python_env\python.exe" -m pip install -r "%DRAGONFLY_PROGRAMDATA%\pythonAllUsersExtensions\Plugins\DSB_efd060071a1711f0b40cf83441a96bd5\requirements.txt"
 
 :: echo( is a newline
 echo(
